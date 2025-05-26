aeb58a10dc3d0ff8845fb1eb1573f7bf2773f750
Cheesz-ball <chenhaonan919@gmail.com>
2025-05-26 17:47:20 +0800

    20250526 analysis

--- backend/analysis/views.py
+++ backend/analysis/views.py
@@ -25,6 +25,7 @@ def dispatcher(request):
 				return JsonResponse({'ret': 1, 'msg': '无效的JSON数据'})
 	print(request.POST)
 	action = request.params.get('action')
+	print(action)
 	if action == 'upload_spectrum':
 		return read_spectrum(request)
 
